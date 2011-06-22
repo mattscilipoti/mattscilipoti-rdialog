2db101288a5e385b8637f023d250e53f255fe5f6
pelgrim <guskald@gmail.com>
2011-06-22 08:19:41 -0300

    Created tests for the the added features

--- test/test_rdialog.rb
+++ test/test_rdialog.rb
@@ -9,3 +9,20 @@ class TestRdialog < Test::Unit::TestCase
     assert true
   end
 end
+
+#Just to keep stuff untouched
+class TestRdialogPlus < Test::Unit::TestCase
+  
+  def setup
+    @dial = RDialog.new
+  end
+  
+  def test_cancel_label
+    @dial.cancellabel = "Cancel label"
+  end
+  
+  def test_ok_label
+    @dial.oklabel = "Ok label"
+  end
+  
+end
